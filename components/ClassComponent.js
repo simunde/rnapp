90c93d39efe97e525127337e92dbee1981d10d23
simunde <sidmundece17@gmail.com>
2025-04-27 16:33:28 -0700

    Functional & class component

---
+++ components/ClassComponent.js
@@ -0,0 +1,14 @@
+import { SafeAreaView, Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export default class ClassComponent extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+      <View>
+        <Text>Class Component</Text>
+      </View>
+      </SafeAreaView>
+    )
+  }
+}
---
+++ components/FunctionalComponet.tsx
@@ -0,0 +1,11 @@
+import {SafeAreaView, Text} from 'react-native';
+
+function FunctionalComponent() {
+  return (
+    <SafeAreaView>
+      <Text>Hello Sid, Have wonderful day!!!!</Text>
+    </SafeAreaView>
+  );
+}
+
+export default FunctionalComponent;
